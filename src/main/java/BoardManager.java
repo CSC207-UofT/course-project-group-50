c01b5a56bb61b69aa693d1937c33aceb320f10a3
Anis Singh <79552425+anissingh@users.noreply.github.com>
2021-10-16 21:09:38 -0400

    Re-wrote some of the skeleton code that didn't push correctly

--- src/main/java/BoardManager.java
+++ src/main/java/BoardManager.java
@@ -1,17 +1,54 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BoardManager {
 
+    private static final int BOARD_SIZE = 24;
     private List<Player> players;
+    private Map<Player, Integer> locations;
+    private Tile[] board;
 
     public BoardManager() {
-        players = new ArrayList<>();
+        this.players = new ArrayList<>();
+        this.board = initializeBoard();
     }
 
     public void addPlayer(String username) {
         Player p = new Player(username);
         players.add(p);
+    }
+
+    public Tile[] initializeBoard() {
+        // create array of tiles
+        return new Tile[BOARD_SIZE];
+    }
+
+    public void initializeLocations() {
+        for(Player p: this.players) {
+            // players start on tile 0
+            this.locations.put(p, 0);
+        }
+    }
+
+    public void updatePos(Player player, int tilesToAdvance) {
+        int tileOfPlayer = advance(player, tilesToAdvance);
+        board[tileOfPlayer].interact();
+    }
+
+    public int advance(Player player, int tilesToAdvance) {
+        int newLoc = (this.locations.get(player) + tilesToAdvance) % BOARD_SIZE;
+        this.locations.put(player, newLoc);
+        return newLoc;
+    }
+
+    public List<Player> getPlayers() {
+        return this.players;
+    }
+
+    public int numOfPlayers() {
+        return this.players.size();
     }
 
     @Override
--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class GameController {
     private BoardManager boardManager;
+    private long id;
+    private final String filepath;
+    private final int netWorthGoal;
+    private final ArrayList<Player> players;
 
-    public GameController(){
+    public GameController(long id, String filepath, ArrayList<Player> players) {
+        this.id = id;
+        this.filepath = filepath;
+        this.players = players;
+        this.netWorthGoal = 5000;
+        this.boardManager = new BoardManager();
+    }
+
+    public GameController(long id, String filepath, ArrayList<Player> players, int netWorthGoal) {
+        this.id = id;
+        this.filepath = filepath;
+        this.players = players;
+        this.netWorthGoal = netWorthGoal;
         this.boardManager = new BoardManager();
     }
 
@@ -14,4 +33,57 @@ public class GameController {
         System.out.println("\n PLAYER LIST");
         System.out.println(boardManager);
     }
+
+    public void runGame() {
+        // not sure where we are getting the usernames from
+        List<String> usernames = new ArrayList<>();
+        runPlayerSetUp(usernames);
+        this.boardManager.initializeLocations();
+        List<Integer> order = generateOrder();
+
+        while(!isWinner()) {
+            for(int i : order) {
+                Player currPlayer = this.boardManager.getPlayers().get(i);
+                int currRoll = currPlayer.roll();
+                this.boardManager.updatePos(currPlayer, currRoll);
+
+            }
+        }
+    }
+
+    public List<Integer> generateOrder() {
+        List<Integer> order = new ArrayList<>();
+        for(int i = 0; i < this.boardManager.numOfPlayers(); i++) {
+            order.add(i);
+        }
+
+        Collections.shuffle(order);
+        return order;
+    }
+
+    public boolean isWinner() {
+        for(Player p : this.boardManager.getPlayers()) {
+            if(p.getNetWorth() == this.netWorthGoal) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // there may be a more efficient way to do this but im unsure how our game loop
+    // will go so i'm just leaving it here
+    public void updateBankruptcy() {
+        for(Player p: this.boardManager.getPlayers()) {
+            if(p.getCash() < -500 || p.getNetWorth() == 0) {
+                p.setBankrupt(true);
+            }
+        }
+    }
+
+    public String getFilepath() {
+        return this.filepath;
+    }
+
+
+
 }
