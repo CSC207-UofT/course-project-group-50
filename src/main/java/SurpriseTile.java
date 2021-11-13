67cc61da0ad0c19d4ab30845809b560b08b7ede4
stevechoi0222 <79773790+stevechoi0222@users.noreply.github.com>
2021-11-12 20:35:12 -0500

    Skeleton SurpriseTile

--- src/main/java/SurpriseTile.java
+++ src/main/java/SurpriseTile.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 public class SurpriseTile extends SpecialTile{
 
-    private Carddeck carddeck;
-
-    public void interact(Player player) {
-        Carddeck carddeck = new Carddeck();
+//    private Carddeck carddeck;
+//
+//    public void interact(Player player) {
+//        Carddeck carddeck = new Carddeck();
     }
-}
