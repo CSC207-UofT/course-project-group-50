7cd683f959f167d2101715875eef743f0b46133b
samrooj <anejasamraj@gmail.com>
2021-11-29 19:09:41 -0500

    empty Gameloop, GameSetUp, Presenter classes which are Controllers

---
+++ src/main/java/controllers/GameLoop.java
@@ -0,0 +1,5 @@
+package controllers;
+
+public class GameLoop {
+    // IDK if we need another class for this so ill leave it empty for now
+}
---
+++ src/main/java/controllers/GameSetUp.java
@@ -0,0 +1,5 @@
+package controllers;
+
+public class GameSetUp {
+    // takes all the user input and initializes the game controller
+}
