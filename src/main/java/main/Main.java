65a1d54fc65ae88c92aa9e70ded728233d69e2b3
Anis Singh <anissingh04@gmail.com>
2021-12-06 02:05:05 -0500

    Created a class that oversees the serialization process so that the serialization occurs in the appropriate layer for clean architecture. Also allows GameSetUp to follow the SRP.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,17 @@
 package main;
 
+import details.GameStateSerializer;
 import interfaceadapters.GameSetUp;
 import details.CmdLineUI;
+import interfaceadapters.SerializerBoundary;
 
 
 public class Main {
     public static void main(String[] args) {
-        GameSetUp gameSetUp = new GameSetUp();
         CmdLineUI ui = new CmdLineUI();
-        gameSetUp.setUpGame(ui);
+        SerializerBoundary serializerBoundary = new GameStateSerializer();
+        GameSetUp gameSetUp = new GameSetUp(ui, serializerBoundary);
+        gameSetUp.setUpGame();
     }
-
 
 }
