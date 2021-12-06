052ee2879e31155b4669cb36552ebda932276e9c
Donghyun Suh <79773846+suhdanny@users.noreply.github.com>
2021-12-05 23:27:14 -0500

    Documentations for Building

--- src/main/java/entities/Building.java
+++ src/main/java/entities/Building.java
@@ -6,12 +6,25 @@ public class Building implements Serializable {
     private final float cost;
     private final float rentIncrease;
 
+    /**
+     * Creates a new Building.
+     * @param cost the cost of this building.
+     * @param rentIncrease the amount of rent increased.
+     */
     public Building(float cost, float rentIncrease) {
         this.cost = cost;
         this.rentIncrease = rentIncrease;
     }
 
+    /**
+     * Returns the cost of this building.
+     * @return The cost of the building.
+     */
     public float getCost() {return this.cost;}
 
+    /**
+     * Returns the amount of rent increased.
+     * @return the amount of rent increased.
+     */
     public float getRentIncrease() {return this.rentIncrease;}
 }
