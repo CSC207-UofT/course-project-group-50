78b1f6e750e4d0731d2b1d064a6b983d4b159195
Donghyun Suh <79773846+suhdanny@users.noreply.github.com>
2021-12-06 01:09:41 -0500

    Token Class Documentations & Additional Fixes

--- src/main/java/entities/StartTile.java
+++ src/main/java/entities/StartTile.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class StartTile extends SpecialTile implements Serializable {
 
     /**
-     * Interact with the tile
+     * Interact with the tile.
      * @param token The token object that is interacting with the tile
      * @param outBound TileOutputBoundary object which allows the tile to interact with the player while following
      *                 clean architecture
